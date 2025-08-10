079e0238f9c02c5eaa8279ff0aa7a59c221701ad
mohammadreza salek <rezasalek75@gmail.com>
2025-08-10 16:27:55 +0400

    disable eslint

--- next.config.js
+++ next.config.js
@@ -2,6 +2,11 @@
 const nextConfig = {
   output: "export", // replaces `next export`
   images: { unoptimized: true },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/i,
