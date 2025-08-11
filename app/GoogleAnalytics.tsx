a3f84ef97202809f8c7e214ca64cef324c616b03
mohammadreza salek <rezasalek75@gmail.com>
2025-08-11 09:44:47 +0400

    add google tag manager

--- app/GoogleAnalytics.tsx
+++ app/GoogleAnalytics.tsx
@@ -2,13 +2,24 @@
 
 import React from "react";
 import Script from "next/script";
+// <!-- Google Tag Manager -->
+// <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+// new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+// j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+// 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+// })(window,document,'script','dataLayer','GTM-WTRMDVJF');</script>
+// <!-- End Google Tag Manager -->
+// <!-- Google Tag Manager (noscript) -->
+// <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WTRMDVJF"
+// height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+// <!-- End Google Tag Manager (noscript) -->
 
 const GoogleAnalytics = () => {
   return (
     <>
-      {/* <Script
+      <Script
         strategy="lazyOnload"
-        src={`https://www.googletagmanager.com/gtag/js?id=G-32FBS7Y85P`}
+        src={`https://www.googletagmanager.com/gtag/js?id=GTM-WTRMDVJF`}
       />
 
       <Script id="" strategy="lazyOnload">
@@ -18,9 +29,9 @@ const GoogleAnalytics = () => {
               function gtag(){dataLayer.push(arguments);}
               gtag('js', new Date());
 
-              gtag('config', 'G-32FBS7Y85P');
+              gtag('config', 'GTM-WTRMDVJF');
           `}
-      </Script> */}
+      </Script>
     </>
   );
 };
